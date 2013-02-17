a007d8163431a65a1d0df57aeed43f1bfdb148a8
luckypool <luckypool314@gmail.com>
2013-02-18 00:29:34 +0900

    Model - modified rank model.

--- motto_idea_web/lib/MottoIdea/Web/jsonrpc/Rank.pm
+++ motto_idea_web/lib/MottoIdea/Web/jsonrpc/Rank.pm
@@ -3,17 +3,7 @@ use Mojo::Base 'MojoX::JSON::RPC::Service';
 use Params::Validate;
 use MottoIdea::Model::Idea::Rank;
 
-use Data::Dumper;
-
 __PACKAGE__->register_rpc_method_names( 'find' );
-
-# SELECT idea_id, tendency
-# FROM ranking_info
-# WHERE tendency >144
-# AND updated_at
-# BETWEEN  '2013-02-11 12:30:00'
-# AND  '2013-02-12 12:31:00'
-# ORDER BY idea_id DESC
 
 sub find {
     my $self = shift;
@@ -23,18 +13,11 @@ sub find {
         order  => { regex => qr/^(DESC|ASC)$/, default => 'DESC' },
     });
     my $model = MottoIdea::Model::Idea::Rank->new;
-    my $find_row = $model->find_recent_top(
+    return $model->find_recent_top(
         limit  => $params->{limit},
         offset => $params->{offset},
+        order => $params->{order},
     );
-    warn Data::Dumper::Dumper $find_row;
-    my $results = $params->{order} eq 'DESC'
-                  ? [ sort { $b->{tendency} <=> $a->{tendency} } @$find_row ]
-                  : [ sort { $a->{tendency} <=> $b->{tendency} } @$find_row ];
-    return $results;
-    # my $sum = 0;
-    # $sum += $_ for @params;
-    # return $sum;
 }
 
 1;
